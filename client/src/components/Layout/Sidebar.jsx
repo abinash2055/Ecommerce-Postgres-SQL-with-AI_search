decea9e4eaca219f98d270d0093357a333168e38
Abinash Nath Pandey <abinashnathpandey@gmail.com>
2026-07-30 22:29:30 +0545

    sidebar 9.10.40

--- client/src/components/Layout/Sidebar.jsx
+++ client/src/components/Layout/Sidebar.jsx
@@ -1,10 +1,9 @@
-// import React from "react";
 import { X, Home, Package, Info, HelpCircle, ShoppingCart, List, Phone } from "lucide-react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
 const Sidebar = () => {
-  return <></>;
+  return (<></>)
 };
 
 export default Sidebar;
