80061b8bf9e143c96bbab694f5533af8bf302f3c
Abinash Nath Pandey <abinashnathpandey@gmail.com>
2026-08-05 16:46:34 +0545

    dashboard 18.00.49

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,39 +1,74 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import SideBar from "./components/SideBar";
 import Login from "./pages/Login";
 import ForgotPassword from "./pages/ForgotPassword";
 import ResetPassword from "./pages/ResetPassword";
 import { ToastContainer } from "react-toastify";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import SideBar from "./components/SideBar";
+import Dashboard from "./components/Dashboard";
+import Orders from "./components/Orders";
+import Users from "./components/Users";
+import Profile from "./components/Profile";
+import Products from "./components/Products";
+
 
 function App() {
 
   const { openedComponent } = useSelector((state) => state.extra);
 
-  const renderDashboardContent = () => { }
+  const { user, isAuthenticated } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
 
-  return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/password/forgot" element={<ForgotPassword />} />
-        <Route path="/password/reset/:token" element={<ResetPassword />} />
 
-        {/* Protected Admin Route */}
-        <Route path="/" element={isAuthenticated && user?.role === "Admin" ? (
-          <div className="flex min-h-screen">
-            <SideBar />
-            {renderDashboardContent()}
-          </div>
-        ) : (
-          <Navigate to="/login" replace />
-        )
-        } />
-      </Routes>
+  const renderDashboardContent = () => {
+    switch (openedComponent) {
+      case "Dashboard":
+        <Dashboard />
+        break;
 
-      <ToastContainer theme="dark" />
-    </Router>
-  );
+      case "Orders":
+        <Orders />
+        break;
+
+      case "Users":
+        <Users />
+        break;
+
+      case "Profile":
+        <Profile />
+        break;
+
+      case "Products":
+        <Products />
+        break;
+
+      default:
+        return <Dashboard />;
+    }
+}
+
+return (
+  <Router>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/password/forgot" element={<ForgotPassword />} />
+      <Route path="/password/reset/:token" element={<ResetPassword />} />
+
+      {/* Protected Admin Route */}
+      <Route path="/" element={isAuthenticated && user?.role === "Admin" ? (
+        <div className="flex min-h-screen">
+          <SideBar />
+          {renderDashboardContent()}
+        </div>
+      ) : (
+        <Navigate to="/login" replace />
+      )
+      } />
+    </Routes>
+
+    <ToastContainer theme="dark" />
+  </Router>
+);
 }
 
 export default App;
